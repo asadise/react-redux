087f80d46b7b5d7844b383d575faa6607c645910
Morteza Asadi <mo_asadi@isc.co.ir>
2020-07-19 13:44:59 +0430

    provide local scope

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src',
     output: {
         path: __dirname + '/build',
@@ -15,12 +16,25 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: "style-loader!css-loader"
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    // use: 'css-loader?modules&importLoaders=true',
+                    use: {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            importLoaders: true,
+                            modules: {
+                                localIdentName: '[path][name]__[local]___[hash:base64:5]'
+                            }
+                        }
+                    }
+
+                }),
             },
         ],
     },
     plugins: [
         new ExtractTextPlugin("styles.css")
     ]
-
 };
